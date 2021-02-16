0ac46fd142b3d7e73cddddea09d8329e5d1ec0ca
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-02-15 23:08:39 -0300

    Service

--- src/main/java/com/matheuscordeiro/ecommercecheckoutapi/servicies/CheckoutServiceImpl.java
+++ src/main/java/com/matheuscordeiro/ecommercecheckoutapi/servicies/CheckoutServiceImpl.java
@@ -5,7 +5,10 @@ import com.matheuscordeiro.ecommercecheckoutapi.enums.Status;
 import com.matheuscordeiro.ecommercecheckoutapi.http.CheckoutRequest;
 import com.matheuscordeiro.ecommercecheckoutapi.repositories.CheckoutRepository;
 import com.matheuscordeiro.ecommercecheckoutapi.servicies.interfaces.CheckoutService;
+import com.matheuscordeiro.ecommercecheckoutapi.streaming.CheckoutCreatedSource;
+import com.matheuscordeiro.ecommercecheckoutapi.event.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,10 +19,19 @@ public class CheckoutServiceImpl implements CheckoutService {
     @Autowired
     CheckoutRepository checkoutRepository;
 
+    @Autowired
+    CheckoutCreatedSource checkoutCreatedSource;
+
     @Override
     public Optional<Checkout> create(CheckoutRequest checkoutRequest) {
         final Checkout checkout = Checkout.builder().code(UUID.randomUUID().toString()).status(Status.CREATED).build();
-        return Optional.of(checkoutRepository.save(checkout));
+        final Checkout entity = checkoutRepository.save(checkout);
+        final CheckoutCreatedEvent checkoutCreatedEvent = CheckoutCreatedEvent.newBuilder()
+                .setCheckoutCode(entity.getCode())
+                .setStatus(entity.getStatus().name())
+                .build();
+        checkoutCreatedSource.output().send(MessageBuilder.withPayload(checkoutCreatedEvent).build());
+        return Optional.of(entity);
     }
 
     @Override
